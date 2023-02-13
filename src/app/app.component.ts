296546470c80b7aeb2d560d928d2b431c5456c01
David Boclé <david.bocle@finastra.com>
2023-02-14 00:49:49 +0100

    refactor navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Personas, PersonaService } from './shared/services/persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   Personas = Personas;
 
-  constructor(public personaService: PersonaService) {}
+  constructor(public personaService: PersonaService, private router: Router) {}
 
   user = {
-    picture: 'https://pbs.twimg.com/profile_images/1550535324501164032/0lTW_4tj_400x400.jpg'
+    picture:
+      'https://pbs.twimg.com/profile_images/1550535324501164032/0lTW_4tj_400x400.jpg',
+  };
+
+  navigateToBpi() {
+    this.router.navigate(['home-bpi']);
   }
 }
--- src/app/features/home-bpi/home-bpi.component.ts
+++ src/app/features/home-bpi/home-bpi.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import { PersonaService } from 'src/app/shared/services/persona.service';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  Personas,
+  PersonaService,
+} from 'src/app/shared/services/persona.service';
 
 @Component({
   selector: 'app-home-bpi',
   templateUrl: './home-bpi.component.html',
-  styleUrls: ['./home-bpi.component.scss']
+  styleUrls: ['./home-bpi.component.scss'],
 })
-export class HomeBpiComponent {
-
+export class HomeBpiComponent implements OnDestroy {
   constructor(public personaService: PersonaService) {
-    this.personaService.setBPI()
+    this.personaService.setBPI();
   }
 
+  ngOnDestroy(): void {
+    this.personaService.setPersona(Personas.BORROWER);
+  }
 }
