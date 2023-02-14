0754bc7263daaa792e4f5e8f6ecb42de8fa75e28
David Boclé <david.bocle@finastra.com>
2023-02-14 03:22:39 +0100

    connect linky via Enedis

--- src/app/features/home/linky/linky.component.ts
+++ src/app/features/home/linky/linky.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+const EnedisEndpoint = 'https://conso.vercel.app/api/auth';
+
 @Component({
   selector: 'app-linky',
   templateUrl: './linky.component.html',
-  styleUrls: ['./linky.component.scss']
+  styleUrls: ['./linky.component.scss'],
 })
 export class LinkyComponent {
-
+  openEnedis() {
+    window.open(EnedisEndpoint, 'Enedis', 'height=500,width=600');
+    return false;
+  }
 }
