44e17331df1bad55516b87cad7e25519d4558b1b
pi5rr5 <sim.pierre91@gmail.com>
2023-02-14 16:21:23 +0100

    yolo 🫰

--- src/app/features/home-bpi/home-bpi.component.ts
+++ src/app/features/home-bpi/home-bpi.component.ts
@@ -49,11 +49,11 @@ export class HomeBpiComponent {
     filter: true,
   };
 
+  public priceTCO2 = 0;
   public countGreenloans = 0;
-  public CO2 = 42;
-  public ratioCO2 = 0.2;
+  public producedCO2 = 0;
   public reducedCO2 = 0;
-  public duration = 5000;
+  public duration = 2000;
 
   // Data that gets displayed in the grid
   public rowData$!: Observable<any[]>;
@@ -68,13 +68,45 @@ export class HomeBpiComponent {
         {
           name: "Consommation",
           type: "column",
-          data: [100, 80, 40, 20],
+          data: [
+            34.424,
+            29.574,
+            26.644,
+            24.632,
+            22.62,
+            20.608,
+            18.596,
+            15.584,
+            14.572,
+            13.56,
+            12.548,
+            12.384,
+            11.536,
+            10.536,
+          ],
           color: "#FFCD00"
         },
+
+
         {
           name: "Objectif 2050",
           type: "line",
-          data: [80, 60, 40, 20],
+          data: [
+            34,
+            29,
+            27,
+            24,
+            22,
+            20,
+            18,
+            16,
+            15,
+            14,
+            13,
+            12,
+            11,
+            10
+          ],
           color: "#00EAAA"
         }
       ],
@@ -88,7 +120,7 @@ export class HomeBpiComponent {
       },
       stroke: {
         width: 6,
-        curve: "smooth"
+        // curve: "smooth"
       },
       plotOptions: {
         bar: {
@@ -96,10 +128,20 @@ export class HomeBpiComponent {
         }
       },
       labels: [
-        "2010",
+        "2020",
+        "2021",
+        "2022",
+        "2023",
+        "2024",
+        "2025",
+        "2026",
+        "2027",
+        "2028",
+        "2029",
         "2030",
-        "2040",
-        "2050",
+        "2031",
+        "2032",
+        "2033",
       ],
       markers: {
         size: 0
@@ -109,7 +151,7 @@ export class HomeBpiComponent {
       },
       yaxis: {
         title: {
-          text: "MWh"
+          text: "GWh"
         },
         min: 0
       },
@@ -119,7 +161,7 @@ export class HomeBpiComponent {
         y: {
           formatter: function (y) {
             if (typeof y !== "undefined") {
-              return y.toFixed(0) + " MWh";
+              return y.toFixed(0) + " GWh";
             }
             return y;
           }
@@ -149,10 +191,27 @@ export class HomeBpiComponent {
   onGridReady(params: GridReadyEvent) {
     this.rowData$ = this.http.get<any[]>('assets/json/greenloans.json');
     this.rowData$.subscribe(res => {
-      this.countGreenloans = res.length
-      // this.reducedCO2 = res.map((item) => Number.parseFloat(item["réalisation (tCO2)"])).reduce((partialSum, a) => partialSum + a, 0.0);
-      this.reducedCO2 = 291;
+      this.countGreenloans = res.length;
+      this.reducedCO2 = this.computeReducedCO2(res);
+      this.producedCO2 = this.computeReducedKWH(res);
+      this.priceTCO2 = this.computePriceTCO2(res, this.reducedCO2);
     });
+  }
+
+  computePriceTCO2(data: any, reducedCO2: any) {
+    const sumInvest = Math.round(data.map((item: any) => Number.parseFloat(item["montant du prêt"])).reduce((pSum: any, a: any) => pSum + a, 0.0));
+    return Math.round(sumInvest / (reducedCO2 * 27));
+  }
+
+  computeReducedCO2(data: any) {
+    return Math.round(data.map((item: any) => Number.parseFloat(item["réalisation (tCO2)"])).reduce((pSum: any, a: any) => pSum + a, 0.0));
+  }
+
+  // SUM(conso energie reference(GWh))-SUM(consommation (GWh))
+  computeReducedKWH(data: any) {
+    const sumEnergieRef = data.map((item: any) => Number.parseFloat(item["conso energie reference(GWh)"])).reduce((pSum: any, a: any) => pSum + a, 0.0);
+    const sumconsommation = data.map((item: any) => Number.parseFloat(item["consommation (GWh/an)"])).reduce((pSum: any, a: any) => pSum + a, 0.0);
+    return Math.round((sumEnergieRef - sumconsommation) * 100) / 100
   }
 
   // Example of consuming Grid Event
